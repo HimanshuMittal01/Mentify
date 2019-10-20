8608f5f9890bfbe5678cbb46f0594c713626fec8
HimanshuMittal01 <44058642+HimanshuMittal01@users.noreply.github.com>
2019-10-20 10:31:51 +0530

    fixed bugs and added calendar page

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:navigation_test/screens/home.dart';
-import 'package:navigation_test/screens/addtodoitem.dart';
-import 'package:navigation_test/screens/todo.dart';
+import 'screen2.dart';
+import 'screen1.dart';
+import 'screen0.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mentify',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Mentify'),
-      routes: <String, WidgetBuilder>{
-        '/addtodoitem': (BuildContext context) => new AddTodoItem(),
-        '/todo': (BuildContext context) => new Todo(),
-//        '/screen3' : (BuildContext context) => new Screen3(),
-//        '/screen4' : (BuildContext context) => new Screen4()
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Screen0(),
+        '/first': (context) => Screen1(),
+        '/second': (context) => Screen2(),
       },
     );
   }
