d5c5dec2c9bdf93ce4114c03401082f55a051f1b
HimanshuMittal01 <44058642+HimanshuMittal01@users.noreply.github.com>
2019-10-20 09:45:57 +0530

    Added project files

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:navigation_test/screens/home.dart';
+import 'package:navigation_test/screens/addtodoitem.dart';
+import 'package:navigation_test/screens/todo.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MLH Hackathon',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'MLH Hackathon'),
+      routes: <String, WidgetBuilder>{
+        '/addtodoitem': (BuildContext context) => new AddTodoItem(),
+        '/todo': (BuildContext context) => new Todo(),
+//        '/screen3' : (BuildContext context) => new Screen3(),
+//        '/screen4' : (BuildContext context) => new Screen4()
+      },
+    );
+  }
+}
